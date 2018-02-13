898f747b4160283e5f369e9b3bee757307683319
Marc Schmitz <marc.schmitz@aixacct.com>
2018-02-13 17:44:12 +0100

    First test for empty board and placing token.

--- TicTacToe_Test/TicTacToe_Test.cpp
+++ TicTacToe_Test/TicTacToe_Test.cpp
@@ -3,8 +3,38 @@
 
 #include "gmock/gmock.h"
 
-TEST(it, works) {
-	ASSERT_TRUE(true);
+class TTTBoard
+{
+	bool mbEmpty{ true };
+public:
+	enum Token {
+		X,
+		O,
+	};
+
+	bool IsEmpty() const {
+		return mbEmpty;
+	}
+
+	void SetToken(enum Token, unsigned int aRow, unsigned int aCol) {
+		mbEmpty = false;
+	}
+};
+
+class ATTTBoard : public ::testing::Test
+{
+public:
+	TTTBoard board;
+};
+
+TEST_F(ATTTBoard, IsEmptyOnCreation) {
+	ASSERT_TRUE(board.IsEmpty());
+}
+
+TEST_F(ATTTBoard, PlaceTokenXOnBoardFillsBoard) {
+	board.SetToken(TTTBoard::Token::X, 1, 1);
+
+	ASSERT_FALSE(board.IsEmpty());
 }
 
 int main(int argc, char** argv)
