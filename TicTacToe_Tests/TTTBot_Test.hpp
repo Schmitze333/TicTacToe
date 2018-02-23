d6ec5d2fd581057f4a05601ec72bddfc0a5c61ba
Marc Schmitz <marcschmitz333@gmail.com>
2018-02-24 00:02:16 +0100

    TTTBot marks non-empty fields with -1.

--- TicTacToe_Tests/TTTBot_Test.hpp
+++ TicTacToe_Tests/TTTBot_Test.hpp
@@ -1,6 +1,7 @@
 #include "gmock/gmock.h"
 
 #include "TTTBoard.h"
+#include <algorithm>
 
 class TTTBot
 {
@@ -10,11 +11,19 @@ public:
   TTTBot(TTTBoard& aBoard) : mBoard(aBoard) {}
 
   std::vector<int> RatingMatrix() const {
-    return std::vector<int>(10, -1);
+    std::vector<TTTBoard::Token> gameState = mBoard.GetGameState();
+    std::vector<int> out;
+    out.resize(gameState.size());
+
+    std::transform(gameState.begin(), gameState.end(), out.begin(),
+        [](TTTBoard::Token t) { return t != TTTBoard::Token::empty ? -1 : 0; }
+        );
+
+    return out;
   }
 };
 
-using ::testing::ElementsAreArray;
+using ::testing::Eq;
 
 class ATTTBot : public ::testing::Test
 {
@@ -22,6 +31,7 @@ public:
   TTTBoard board;
   TTTBoard::Token X{ TTTBoard::Token::X };
   TTTBoard::Token O{ TTTBoard::Token::O };
+  TTTBoard::Token E{ TTTBoard::Token::empty };
 };
 
 TEST_F(ATTTBot, RatingMatrixForFullSetBoardIsAllTaken)
@@ -32,6 +42,25 @@ TEST_F(ATTTBot, RatingMatrixForFullSetBoardIsAllTaken)
       O, X, O
       });
 
+  std::vector<int> expected({
+      -1, -1, -1,
+      -1, -1, -1,
+      -1, -1, -1
+  });
+
   TTTBot bot(board);
-  ASSERT_THAT(bot.RatingMatrix(), ElementsAreArray({-1, -1, -1, -1, -1, -1, -1, -1, -1, -1}));
+  ASSERT_THAT(bot.RatingMatrix(), Eq(expected));
+}
+
+TEST_F(ATTTBot, RatinMatrixSetsNonEmptyFieldToMinusOne)
+{
+  board.SetGameState({
+      X, E, E,
+      E, E, E,
+      E, E, E
+      });
+
+  TTTBot bot(board);
+  ASSERT_THAT(bot.RatingMatrix().at(0), Eq(-1));
+  ASSERT_THAT(bot.RatingMatrix().at(1), ::testing::Ne(-1));
 }
